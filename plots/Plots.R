8bf352194335fe323f3395457f6794c61cee06d6
Rishabh Garg <rishabhgarg@Rishabhs-Air.attlocal.net>
2025-07-18 17:42:16 -0700

    Edited out unnecessary code

--- plots/Plots.R
+++ plots/Plots.R
@@ -1,23 +1,23 @@
 
 ##--------------- ANALYSIS FOR CESA_EXTERNAL (external data)-----------
-data_external <- biological_weights_external[, .(total_snvs, SBS4, SBS5, SBS13, SBS87, SBS24)]
-data_external <- data_external[complete.cases(data_external),]
-plot_TMB_binary_boxplot(data_external, "SBS4", 0.25, "(Binary for Signature Presence: 0.25)")
-plot_TMB_binary_boxplot(data_external, "SBS13", 0.25, "(Binary for Signature Presence: 0.25)")
-plot_TMB_binary_boxplot(data_external, "SBS5", 0.25, "(Binary for Signature Presence: 0.25)")
-
-# Plot a linear regression of log(TMB) vs Mutations Attributed to SBS4 and SBS13 (external data)
-external_TMB_linreg_SBS4 <- plot_TMB_logistic_regression(biological_weights_external, "external", "SBS4")
-external_TMB_linreg_SBS13 <- plot_TMB_logistic_regression(biological_weights_external, "external", "SBS13")
-external_TMB_linreg_SBS5 <- plot_TMB_logistic_regression(biological_weights_external, "external", "SBS5")
+# data_external <- biological_weights_external[, .(total_snvs, SBS4, SBS5, SBS13, SBS87, SBS24)]
+# data_external <- data_external[complete.cases(data_external),]
+# plot_TMB_binary_boxplot(data_external, "SBS4", 0.25, "(Binary for Signature Presence: 0.25)")
+# plot_TMB_binary_boxplot(data_external, "SBS13", 0.25, "(Binary for Signature Presence: 0.25)")
+# plot_TMB_binary_boxplot(data_external, "SBS5", 0.25, "(Binary for Signature Presence: 0.25)")
+# 
+# # Plot a linear regression of log(TMB) vs Mutations Attributed to SBS4 and SBS13 (external data)
+# external_TMB_linreg_SBS4 <- plot_TMB_logistic_regression(biological_weights_external, "external", "SBS4")
+# external_TMB_linreg_SBS13 <- plot_TMB_logistic_regression(biological_weights_external, "external", "SBS13")
+# external_TMB_linreg_SBS5 <- plot_TMB_logistic_regression(biological_weights_external, "external", "SBS5")
 
 # Plot Survival Curves 
 
 # Signature Absence (<0.25 or <paper_bs_optimal_cutoff) vs Presence (>= 0.25 or >=paper_bs_optimal_cutoff)
 
-SBS5_results$external
-SBS4_results$external
-SBS13_results$external
+# SBS5_results$external
+# SBS4_results$external
+# SBS13_results$external
 
 SBS4_survival_0.25_external <- SBS4_results$external$plot_0.25
 SBS13_survival_0.25_external <- SBS13_results$external$plot_0.25
@@ -30,19 +30,18 @@ data_paper <- biological_weights_paper[, .(total_snvs, SBS3, SBS4, SBS5, SBS13, 
 data_paper <- data_paper[complete.cases(data_paper),]
 
 SBS4_0.25 <- plot_TMB_binary_boxplot(data_paper, "SBS4", 0.25, "(Binary for Signature Presence: 0.25)")
-SBS4_0.25
 SBS4_optimal <- plot_TMB_binary_boxplot(data_paper, "SBS4", SBS4_survival_optimal_cutoff_paper_mean, paste0("(Optimal Binary for Signature Presence: ",SBS4_survival_optimal_cutoff_paper_mean,")"))
-SBS4_optimal
 SBS13_0.25 <- plot_TMB_binary_boxplot(data_paper, "SBS13", 0.25, "(Binary for Signature Presence: 0.25)")
-SBS13_0.25
 SBS13_optimal <- plot_TMB_binary_boxplot(data_paper, "SBS13", SBS13_survival_optimal_cutoff_paper_mean, paste0("(Optimal Binary for Signature Presence: ",SBS13_survival_optimal_cutoff_paper_mean,")"))
-SBS13_optimal
-plot_TMB_binary_boxplot(data_paper, "SBS5", 0.25, "(Binary for Signature Presence: 0.25)")
 
-
-# to look more into:
-SBS87_0.25 <- plot_TMB_binary_boxplot(data_paper, "SBS87", SBS87_paper_optimal_TMB_cutoff_for_paper_non_bs$estimate, "(Binary for Signature Presence: 0.25)")
-SBS24_0.25 <- plot_TMB_binary_boxplot(data_paper, "SBS24", 0.21, "(Binary for Signature Presence: 0.25)")
+# SBS4_0.25
+# SBS4_optimal
+# SBS13_0.25
+# SBS13_optimal
+# plot_TMB_binary_boxplot(data_paper, "SBS5", 0.25, "(Binary for Signature Presence: 0.25)")
+# # to look more into:
+# SBS87_0.25 <- plot_TMB_binary_boxplot(data_paper, "SBS87", SBS87_paper_optimal_TMB_cutoff_for_paper_non_bs$estimate, "(Binary for Signature Presence: 0.25)")
+# SBS24_0.25 <- plot_TMB_binary_boxplot(data_paper, "SBS24", 0.21, "(Binary for Signature Presence: 0.25)")
 
 # Convert data to data.frames for ggplot2
 sbs13_tmb_df <- data.frame(cutoff = SBS13_TMB_optimal_cutoff_paper[[1]])
@@ -111,10 +110,11 @@ data_paper_sig <- paper_signatures_merged[, .(total_snvs, SBS4, SBS5, SBS13)]
 data_paper_sig <- data_paper_sig[complete.cases(data_paper_sig),]
 
 SBS4_0.25_cutoff_paper_signatures <- plot_TMB_binary_boxplot(data_paper_sig, "SBS4", 0.25, "(Binary for Signature Presence: 0.25)")
-SBS4_0.25_cutoff_paper_signatures
 SBS13_0.25_cutoff_paper_signatures <- plot_TMB_binary_boxplot(data_paper_sig, "SBS13", 0.25, "(Binary for Signature Presence: 0.25)")
-SBS13_0.25_cutoff_paper_signatures
-plot_TMB_binary_boxplot(data_paper_sig, "SBS5", 0.25, "(Binary for Signature Presence: 0.25)")
+
+# SBS4_0.25_cutoff_paper_signatures
+# SBS13_0.25_cutoff_paper_signatures
+# plot_TMB_binary_boxplot(data_paper_sig, "SBS5", 0.25, "(Binary for Signature Presence: 0.25)")
 
 # Plot a linear regression of log(TMB) vs Mutations Attributed to SBS4 and SBS13 (paper data)
 
@@ -127,43 +127,45 @@ paper_TMB_linreg_SBS24 <- plot_TMB_logistic_regression(biological_weights_paper,
 
 # Signature Absence (<0.25 or <paper_bs_optimal_cutoff) vs Presence (>= 0.25 or >=paper_bs_optimal_cutoff)
 
-SBS5_results$paper$plot_0.25
+# SBS5_results$paper$plot_0.25
 SBS4_survival_0.25 <- SBS4_results$paper$plot_0.25
 SBS4_survival_optimal <- SBS4_results$paper$plot_optimal
 
 SBS13_survival_0.25 <- SBS13_results$paper$plot_0.25
 SBS13_survival_optimal <- SBS13_results$paper$plot_optimal
 
-SBS4_survival_0.25
-SBS4_survival_optimal
+
 SBS4_0.25_cutoff_survival_paper_signatures <- sig_result_SBS4$plot_0.25 # Li et al mutational activities
-SBS4_0.25_cutoff_survival_paper_signatures
-SBS13_survival_0.25
-SBS13_survival_optimal
 SBS13_0.25_cutoff_survival_paper_signatures <- sig_result_SBS13$plot_0.25 # Li et al mutational activities
-SBS13_0.25_cutoff_survival_paper_signatures
+
+# SBS4_survival_0.25
+# SBS4_survival_optimal
+# SBS4_0.25_cutoff_survival_paper_signatures
+# SBS13_survival_0.25
+# SBS13_survival_optimal
+# SBS13_0.25_cutoff_survival_paper_signatures
 
 # Mutational Signature Analysis
 
 diverging_plot_sig_comparison <- create_diverging_signature_plot(Li_data = paper_signatures, ref_based_data = biological_weights_paper)
-diverging_plot_sig_comparison
+# diverging_plot_sig_comparison
 
 ##--------------- ANALYSIS FOR CESA_ALL (all data)-----
-data_all <- biological_weights_all[, .(total_snvs, SBS4, SBS5, SBS13)]
-data_all <- data_all[complete.cases(data_all),]
-plot_TMB_binary_boxplot(data_all, "SBS4", 0.25, "(Binary for Signature Presence: 0.25)")
-plot_TMB_binary_boxplot(data_all, "SBS13", 0.25, "(Binary for Signature Presence: 0.25)")
-plot_TMB_binary_boxplot(data_all, "SBS13", SBS13_survival_optimal_cutoff_paper_mean, paste0("(Optimal Binary for Signature Presence: ",SBS13_survival_optimal_cutoff_paper_mean,")"))
-plot_TMB_binary_boxplot(data_all, "SBS5", 0.25, "(Binary for Signature Presence: 0.25)")
-
-# Plot a linear regression of log(TMB) vs Mutations Attributed to SBS4 and SBS13 (all data)
-all_TMB_linreg_SBS4 <- plot_TMB_logistic_regression(biological_weights_all, "all", "SBS4")
-all_TMB_linreg_SBS13 <- plot_TMB_logistic_regression(biological_weights_all, "all", "SBS13")
-all_TMB_linreg_SBS5 <- plot_TMB_logistic_regression(biological_weights_all, "all", "SBS5")
-# Plot Overall Survival Curves 
-
-# Signature Absence (<0.25 or <paper_bs_optimal_cutoff) vs Presence (>= 0.25 or >=paper_bs_optimal_cutoff)
-
-SBS5_results$all
-SBS4_results$all
-SBS13_results$all
+# data_all <- biological_weights_all[, .(total_snvs, SBS4, SBS5, SBS13)]
+# data_all <- data_all[complete.cases(data_all),]
+# plot_TMB_binary_boxplot(data_all, "SBS4", 0.25, "(Binary for Signature Presence: 0.25)")
+# plot_TMB_binary_boxplot(data_all, "SBS13", 0.25, "(Binary for Signature Presence: 0.25)")
+# plot_TMB_binary_boxplot(data_all, "SBS13", SBS13_survival_optimal_cutoff_paper_mean, paste0("(Optimal Binary for Signature Presence: ",SBS13_survival_optimal_cutoff_paper_mean,")"))
+# plot_TMB_binary_boxplot(data_all, "SBS5", 0.25, "(Binary for Signature Presence: 0.25)")
+# 
+# # Plot a linear regression of log(TMB) vs Mutations Attributed to SBS4 and SBS13 (all data)
+# all_TMB_linreg_SBS4 <- plot_TMB_logistic_regression(biological_weights_all, "all", "SBS4")
+# all_TMB_linreg_SBS13 <- plot_TMB_logistic_regression(biological_weights_all, "all", "SBS13")
+# all_TMB_linreg_SBS5 <- plot_TMB_logistic_regression(biological_weights_all, "all", "SBS5")
+# # Plot Overall Survival Curves 
+# 
+# # Signature Absence (<0.25 or <paper_bs_optimal_cutoff) vs Presence (>= 0.25 or >=paper_bs_optimal_cutoff)
+# 
+# SBS5_results$all
+# SBS4_results$all
+# SBS13_results$all
